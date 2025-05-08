a45701910a76adc9678fa75834adddb58a07a5e6
sh2099 <samhewson16@gmail.com>
2025-05-08 14:39:56 +0200

    resolve initial teething issues

--- ml/train_nn.py
+++ ml/train_nn.py
@@ -1,25 +1,25 @@
 import logging
 
 from hydra import main
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from ml.training import run_training
 from ml.utils import pick_device
 
-logging = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-@main(config_path="../configs/neural", config_name="config_nn")
+@main(config_path="../configs/ml", config_name="config")
 def cli(cfg: DictConfig) -> None:
     logging.basicConfig(level=cfg.logging.level)
-    logging.info("Starting neural‐net training with config:\n%s", cfg.pretty())
+    logger.info("Starting neural‐net training with config:\n%s", OmegaConf.to_yaml(cfg))
 
     device = pick_device(
         use_cuda=cfg.hardware.use_cuda,
         use_dml=cfg.hardware.use_dml,
         use_mps=cfg.hardware.use_mps,
     )
-    logging.info("Using device %s", device)
+    logger.info("Using device %s", device)
 
     run_training(cfg, device)
 
